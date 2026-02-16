8064feaf6c9aee1a459d36f9c741ac06d3d35b19
mark346maxik-maker <mark346maxik@gmail.com>
2026-02-16 17:33:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 # ========== СИСТЕМА ДОСТУПА ==========
-ALLOWED_USERS = [1948172415,878048905]  # 🔹 Ваш ID
+ALLOWED_USERS = [1948172415,878048905,751640039]  # 🔹 Ваш ID
 
 def check_access(user_id):
     return user_id in ALLOWED_USERS
